d69fdc310013fcd7ff5b1655759d10daf45d4add
root <root@paradox.(none)>
2015-07-24 11:15:18 +0200

    adding bank B

--- mcplib/mcp23017.py
+++ mcplib/mcp23017.py
@@ -1,27 +1,28 @@
+
 class MCP23017(object):
 
 	def __init__(self):
-		self.ports = 0
-
+		self.ports = [0,0]
+		
 	def p2add(self, port):
 		return pow(2, port)
 
 	def cleanup(self):
-		self.ports = 0
+		self.ports = [0, 0]
 		# send 0 to port
 
-	def set(self, port, value):
+	def set(self, bank, port, value):
 		if value:
-			self.ports = self.ports | self.p2add(port)
+			self.ports[bank] = self.ports[bank] | self.p2add(port)
 		else:
-			self.ports = self.ports & ~ self.p2add(port)
+			self.ports[bank] = self.ports[bank] & ~ self.p2add(port)
 
-		print " %s to hardware "%self.ports
+		print " write bank %s , val: %s to hardware "%(bank, self.ports[bank])
 		# set hardware = self.ports
 
-	def get(self, port):
+	def get(self, bank, port):
 		# read port p2add(port)
-		return bool(self.ports & self.p2add(port))
+		return bool(self.ports[bank] & self.p2add(port))
 
 
 
--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from mcplib import MCP23017
 
+_A = 0
+_B = 1
 chip = MCP23017()
 
 #chip.cleanup()
 
-chip.set(1,True)
-print "---"
-chip.set(0,True)
-chip.set(1,False)
-chip.set(7,True)
+chip.set(_A,1,True)
+chip.set(_A,0,True)
+chip.set(_A,1,False)
+chip.set(_A,7,True)
 
+print "p#\tA\tB"
 for i in range (0,8):
-	print "%s:%s" %(i, chip.get(i))
+	print "%s\t%s\t%s" %(i, chip.get(_A,i), chip.get(_B,i))
